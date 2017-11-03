61d89e31f8fe3613bea07b38a5fc55c5102b9821
John <johnedwardsawiris@gmail.com>
2017-11-03 13:20:51 -0700

    add validation code on the frontend

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@ function Pizza(topping, size) {
   this.size = size;
 }
 Pizza.prototype.calculatePrice = function() {
+ var price;
+
   if (this.size === 0) {
 
     if (this.topping.length === 1) {
@@ -29,35 +31,42 @@ Pizza.prototype.calculatePrice = function() {
     }
 
   } else {
+
     if (this.topping.length === 1) {
-      return price = "Price is " + 12;
-    } else if (this.topping.length === 2) {
-      return price = "Price is " + 14;
-    } else if (this.topping.length === 3) {
-      return price = "Price is " + 16;
-    } else {
-      return price = "Price is " + 20;
-    }
-  }
+       return price = "Price is " + 12;
+     } else if (this.topping.length === 2) {
+       return price = "Price is " + 14;
+     } else if (this.topping.length === 3) {
+       return price = "Price is " + 16;
+     } else if (this.topping.length > 3){
+       return  price = "Price is " + 20;
+     }
+   }
 }
 
 //Frontend Logic
 $(document).ready(function() {
   var toppings = [];
   var size;
+
   $("#order").submit(function(event) {
     event.preventDefault();
-    if($("input[type=radio]").is(":checked")) {
+    if ($("input[type=radio]").is(":checked") && $("input[type=checkbox]").is(":checked")) {
       size = parseInt($("input[type=radio]:checked").val());
       $("input[type=checkbox]:checked").each(function() {
-      toppings.push($(this).attr("value"));
+        toppings.push($(this).attr("value"));
+        console.log(toppings)
       });
-    } else {
-      alert("selected a size")
-    }
-    var newOrder = new Pizza(toppings, size);
+    } else if (!$("input[type=checkbox]").is(":checked") || !$("input[type=radio]").is(":checked")) {
+        alert("Pick size and topping");
+      }
     // $(this).fadeOut(800);
-    alert(newOrder.calculatePrice());
+    var newOrder = new Pizza(toppings, size);
+    $("#price").text(newOrder.calculatePrice());
   });//Submit ends
+  $("#reset").click(function() {
+    toppings = [];
+    $("#price").empty();
+  });//reset click ends
 
 });//Ready ends
